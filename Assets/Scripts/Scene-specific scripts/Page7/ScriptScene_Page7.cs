4f0d682b28b651c7f4e8e8a12014fbcf9659882f
Andrey Chegodaev <chegodaevandrey@yandex.ru>
2023-01-17 17:12:27 -0800

    Lvl 3

--- Assets/Scripts/Scene-specific scripts/Page7/ScriptScene_Page7.cs
+++ Assets/Scripts/Scene-specific scripts/Page7/ScriptScene_Page7.cs
@@ -4,72 +4,31 @@ using UnityEngine;
 
 public class ScriptScene_Page7 : MonoBehaviour
 {
-    public GameObject realPrincess;
-
-    public Transform princessStartPosition; // have to do it because of a bug with Full sreen 
-    public Transform[] princessPositions;
+    public string[] animationNames;
     public Camera illustrationCamera;
-
-    private int currentPrincessPosition;
-    private float princessSpeed;
-    private Animator anim;
+    int currentAnimation;
+    Animator anim;
 
 
+    // Start is called before the first frame update
     void Start()
     {
+
         anim = gameObject.GetComponent<Animator>();
-        princessSpeed = realPrincess.GetComponent<MoveToClick>().speed;
-        currentPrincessPosition = 0;
-        transform.position = princessStartPosition.position;
-    }
-    private void Update()
-    {
-
-
-        if (currentPrincessPosition == 0)
-        {
-            anim.SetFloat("Speed", 70);
-            anim.SetFloat("Horizontal", 0.5f);
-            anim.SetFloat("Vertical", 0.5f);
-        }
-
-        else if (currentPrincessPosition == 1)
-        {
-            anim.SetBool("CrossRavine", true);
-
-        }
-
-        else if (currentPrincessPosition == 2)
-        {
-            anim.SetFloat("Speed", 70);
-            anim.SetFloat("Horizontal", 0.5f);
-            anim.SetFloat("Vertical", 0.5f);
-        }
-
-        if (transform.position == princessPositions[currentPrincessPosition].position)
-        {
-            anim.SetFloat("Speed", 0);
-        }
-
+        currentAnimation = 0;
+        anim.Play(animationNames[currentAnimation]);
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (currentPrincessPosition <= princessPositions.Length - 1)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, princessPositions[currentPrincessPosition].position, princessSpeed * Time.deltaTime);
-
-        }
-
-    }
 
     public void TaskOnClick()
     {
-        if (currentPrincessPosition <= princessPositions.Length - 1)
+        if (currentAnimation <= animationNames.Length - 1)
         {
-            transform.position = princessPositions[currentPrincessPosition].transform.position;
-            currentPrincessPosition++;
+            currentAnimation++;
+            anim.Play(animationNames[currentAnimation]);
         }
+
     }
+
+
 }
